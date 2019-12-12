f5ee53dd2854c4bba049163b9b2b0cc47d7b3d91
Brian Ruiz <brianruiz123@yahoo.com>
2019-12-11 22:36:06 -0600

    Imported dataset

--- yourFirstMap.py
+++ yourFirstMap.py
@@ -1,5 +1,20 @@
-# Local installation: http://geopandas.org/install.html
-#%% 
-import geopandas as gpd
+# Introduction
 
-#%%
+"""Kiva is an online crowdfunding platform extending financial services to poor people around the world.
+Kiva lenders have provided over $1 billion dollars in loans to over 2 million people.
+
+In this exercise, you'll investigate Kiva loans in the Philippines.
+Can you identify regions that might be outside of Kiva's current network, in order to identify opportunities
+for recruiting new Field Partners?
+"""
+
+#%%  Importing requisite modules
+import geopandas as pd
+
+from learntools.core import binder
+binder.bind(globals())
+from learntools.geospatial.ex1 import *
+
+#%% Reading in the data
+loans_filepath = "../input/geospatial-learn-course-data/kiva_loans/kiva_loans/kiva_loans.shp"
+
